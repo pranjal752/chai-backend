2facdc64780a0e7e7bb658e13459a9721c210ee2
Pranjal Singh <your-email@example.com>
2026-09-20 00:28:39 +0530

    Fix typo in subscription model schema and export statement

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,6 +1,6 @@
 import mongoose, {Schema} from "mongoose"
 
-const subsciptionSchema = new Schema({
+const subscriptionSchema = new Schema({
     subscriber: {
         type: Schema.Types.ObjectId, // one who is subscribing
         ref: "User"
@@ -11,4 +11,4 @@ const subsciptionSchema = new Schema({
     }
 }, {timeStamps: true})
 
-export const Subsciption = mongoose.model("Subsciption", SubsciptionSchema)
+export const Subscription = mongoose.model("Subscription", SubscriptionSchema)
