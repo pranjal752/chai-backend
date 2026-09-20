3ec9e39c0227a8922cd01ed65c2b708fb8e7fdb7
Pranjal Singh <your-email@example.com>
2026-09-21 01:38:33 +0530

    Add getUserChannelProfile function to fetch user channel details and subscription status

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -263,6 +263,77 @@ const updateUserCoverImage = asyncHandler(asserts(req, res) => {
   )
 })
 
+const getUserChannelProfile = asyncHandler(async(req, res) => {
+  const {userName} = req.params
+
+  if (!userName?.trim()) {
+    throw new ApiError(400, "username is missing")
+  }
+
+  const channel = await User.aggregate([
+    {
+      $match: {
+        username: username?.toLowerCase()
+      }
+    },
+    {
+      $lookup: {
+        from: "Subscriptions" ,
+        localField: "_id" ,
+        foriegnField: "channel" , 
+        as: "subscribers"
+      }
+    },
+    {
+      $lookup: {
+         from: "Subscriptions" ,
+         localField: "_id" ,
+         foriegnField: "subscriber" , 
+         as: "subscribedTo"
+      }
+    },
+    {
+      $addFields: {
+          subscribersCount: {
+            $size: "$subscribers"
+          },
+          channelsSubscribedToCount: {
+            $size: "$subscribedTo"
+          },
+          isSubscribed: {
+            $cond: {
+              if: {$in: [req.user?._id, "$subscribers.subscriber"]},
+              then: true,
+              else: false
+            }
+          }
+      }
+    },
+    {
+      $project: {
+        fullName: 1,
+        userName: 1,
+        subscribersCount: 1,
+        isSubscribed: 1,
+        avatar: 1,
+        coverImage: 1,
+        email: 1 
+
+      }
+    }
+  ]) 
+
+  if (!channel?.length) {
+    throw new ApiError(404, "channel does not exist")
+  }
+
+  return res
+  .status(200)
+  .json(
+    new ApiResponse(200,channel[0], "User channel fetched successfully")
+  )
+})
+
 export {
   registerUser,
   loginUser,
@@ -271,7 +342,8 @@ export {
   changeCurrentPassword,
   getCurrentUser,
   updateAccountDetails,
-  updateUserCoverImage
+  updateUserCoverImage,
+  getUserChannelProfile
 }
 
 
